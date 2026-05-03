3f9b427d60f4187c5611021bdb6eba990122b005
Marco Gilot <marcogilot36@gmail.com>
2026-05-03 10:56:08 +0100

    feat: handle nuke menu creation internally

--- Template_Manager/menu.py
+++ Template_Manager/menu.py
@@ -2,6 +2,6 @@ import nuke
 from .main import launch_ui
 
 def setup_menu():
-    nuke.menu("Nuke").addMenu("Pipeline Tools").addCommand("Template Manager", launch_ui, "ctrl+t")
+    nuke.menu("Nuke").addMenu("Template Manager").addCommand("Start Template Manager", launch_ui, "ctrl+t")
 
 setup_menu()
